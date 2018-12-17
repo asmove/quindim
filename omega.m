642567d5d748db7a125b54a5c1ce45ee8c7546d0
brunolnetto <brunolnetto@gmail.com>
2018-12-17 01:27:44 -0300

    Kinematic improvements for the velocity and omega

--- omega.m
+++ omega.m
@@ -1,17 +1,6 @@
-function omega = omega(T, vars, diffs)
-    syms t;
-
-    T_ = formula(T);
-    R = T_(1:3, 1:3);
-    
-    Rp = diff(R, t);    
-     
-    Rp = subs(Rp, diffs, vars);
-    
+function omega = omega(R, q, qp)
+    Rp = dmatdt(R, q, qp);
     Somega = Rp*R.';
-    Somega = formula(Somega);
-
     omega = unskew(Somega);
-
     omega = simplify(omega);
 end
--- velocity.m
+++ velocity.m
@@ -1,6 +1,5 @@
 function v = velocity(Tp, d)
     v_ = Tp*[d; 1];
     v = formula(v_);
-    v = simplify(v);
     v = v(1:3);
 end
