542aa4a876356de745bb1a943caa160f6c1c4c3d
Bruno Peixoto <brunolnetto@gmail.com>
2019-03-07 08:21:00 -0300

    Mistaken dot

--- kinematics/rot_quaternion.m
+++ kinematics/rot_quaternion.m
@@ -1,7 +1,7 @@
 function R = rot_quaternion(q)
 
     % Norm of q
-    s = norm(q);
+    s = simplify(norm(q));
     
     % q = qr + qx*i + qy*j + qz*k
     q_ = formula(sym(q));
@@ -12,7 +12,7 @@ function R = rot_quaternion(q)
     qz = q_(4);
     
     % Rotation matrix
-    R = [1 - 2*s*(qy^.2 + qz.^2),     2*s*(qx*qy - qz*qr),     2*s*(qx*qz + qy*qr); ...
+    R = [1 - 2*s*(qy.^2 + qz.^2),     2*s*(qx*qy - qz*qr),     2*s*(qx*qz + qy*qr); ...
              2*s*(qx*qy + qz*qr), 1 - 2*s*(qx^.2 + qz.^2),     2*s*(qy*qz - qx*qr);  ...
              2*s*(qx*qz - qy*qr),     2*s*(qy*qz + qx*qr), 1 - 2*s*(qx^.2 + qy.^2)];
          
