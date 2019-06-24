b31a535e1934edad99396e09976713af1551362b
brunolnetto <brunolnetto@gmail.com>
2019-06-24 09:28:46 -0300

    Cholesky decomposition

--- dynamics/dynamic_model.m
+++ dynamics/dynamic_model.m
@@ -11,7 +11,10 @@ function sys = dynamic_model(sys, method)
     
     % Main matrices
     [sys.M, sys.g, sys.friction, sys.nu, sys.U, ...
-     sys.H, sys.h, sys.Z] =  dyn_matrices(sys);
+     sys.H, sys.h, sys.Z, sys.W] =  dyn_matrices(sys);
+    
+    % Quasi-velocities
+    sys.quasi_vel = sys.W.'*sys.qp;
  
     % Sytem behaviour
     qpps = sys.H\(sys.Z*sys.u - sys.h);
