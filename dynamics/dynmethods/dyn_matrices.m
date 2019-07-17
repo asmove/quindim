22db5758ed951c3ce37e2e8c07e2c8667f101efa
Bruno Peixoto <brunolnetto@gmail.com>
2019-07-17 10:14:22 -0300

    SImplification issues

--- dynamics/dynmethods/dyn_matrices.m
+++ dynamics/dynmethods/dyn_matrices.m
@@ -2,18 +2,18 @@ function sys = dyn_matrices(sys)
     u = sys.u;
     
     % Dynamic matrices of a mechanical system
-    sys.dyn.M = mass_matrix(sys);
-    sys.dyn.g = gravitational(sys);
-    sys.dyn.f = friction(sys);
-    sys.dyn.U = -jacobian(sys.dyn.l_r, u);
+    sys.dyn.M = simplify_(mass_matrix(sys), 10);
+    sys.dyn.g = simplify_(gravitational(sys), 10);
+    sys.dyn.f = simplify_(friction(sys), 10);
+    sys.dyn.U = simplify_(-jacobian(sys.dyn.l_r, u), 10);
 
-    sys.dyn.nu = simplify(sys.dyn.l_r - sys.dyn.M*sys.pp, 'Seconds', 10) - ...
+    sys.dyn.nu = simplify_(sys.dyn.l_r - sys.dyn.M*sys.pp, 10) - ...
                           sys.dyn.g - sys.dyn.f + sys.dyn.U*sys.u;
     
     % Control dynamic matrices
     sys.dyn.H = sys.dyn.M;
-    sys.dyn.h = simplify(sys.dyn.nu + sys.dyn.g + sys.dyn.f, 'Seconds', 10);
-    sys.dyn.Z = sys.dyn.U;
+    sys.dyn.h = simplify_(sys.dyn.nu + sys.dyn.g + sys.dyn.f, 10);
+    sys.dyn.Z = simplify_(sys.dyn.U);
     
-    sys.dyn.W = chol(sys.dyn.H, 'lower', 'nocheck');
+    sys.dyn.W = simplify_(chol(sys.dyn.H, 'lower', 'nocheck'), 10);
 end
