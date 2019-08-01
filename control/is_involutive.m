4c8f951cbfec0a724463292535e66a09020b3ceb
Bruno Peixoto <brunolnetto@gmail.com>
2019-08-01 09:25:12 -0300

    Involutivity proof

--- control/is_involutive.m
+++ control/is_involutive.m
@@ -1,12 +1,12 @@
-function is_invol = is_involutive(fs)
+function is_invol = is_involutive(fs, x)
     [~, m] = size(fs);
     
     r_fs = rank(simplify_(fs));
     
     for i = 1:m
         for j = i+1:m
-            lie_fi_fj = lie_bracket(fs(i), fs(j));
-            r_fs_lie = rank(simplify_(fs, lie_fi_fj));
+            lie_fi_fj = lie_bracket(fs(i), fs(j), x);
+            r_fs_lie = rank(simplify_([fs, lie_fi_fj]));
             
             if(r_fs_lie ~= r_fs)
                 is_invol = false;
