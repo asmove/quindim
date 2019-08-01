67983f808091812edc23bb0c4d0e276f1184f339
Bruno Peixoto <brunolnetto@gmail.com>
2019-08-01 09:25:34 -0300

    Model simulation script

--- simulation/sim_model.m
+++ simulation/sim_model.m
@@ -1,6 +1,8 @@
-function simOut = sim_model(mdlname)
+function simOut = sim_model(mdlname, tf)
     open_system(mdlname);
     set_param(mdlname, 'SaveOutput', 'on');
-    simOut = sim(mdlname, 'AbsTol', '1e-5', 'RelTol', '1e-5');
+    simOut = sim(mdlname, 'AbsTol', '1e-5', ...
+                          'RelTol', '1e-5', ...
+                          'StopTime', num2str(tf));
     close_system(mdlname);
 end
