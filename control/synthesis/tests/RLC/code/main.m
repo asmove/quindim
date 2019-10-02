7e2551a3042e676bf2650f06610d2548183eb077
brunolnetto <brunolnetto@gmail.com>
2019-10-02 13:15:20 -0300

    Multiplos Phis, sats, imprecision factors

--- control/synthesis/tests/RLC/code/main.m
+++ control/synthesis/tests/RLC/code/main.m
@@ -1,13 +1,18 @@
-clear all
-close all
-clc
+% clear all
+% close all
+% clc
+% 
+% run('~/github/Robotics4fun/examples/RLC/code/main.m');
 
 phi_opts = [1, 10, 100];
 is_sat_opts = [true, false];
+is_imprecises = [true, false];
 
 for phi = phi_opts
     for is_sat = is_sat_opts
-        run('~/github/Robotics4fun/examples/RLC/code/main.m');
+        for is_imprecise = is_imprecises
+            run('./RLC.m')
+        end
     end
 end
 
--- control/synthesis/tests/masses/code/main.m
+++ control/synthesis/tests/masses/code/main.m
@@ -1,13 +1,13 @@
-clear all
-close all
-clc
-
-run('~/github/Robotics4fun/examples/1_mass/code/main.m');
+% clear all
+% close all
+% clc
+% 
+% run('~/github/Robotics4fun/examples/1_mass/code/main.m');
 
 percs = [0, 0.5];
 is_sat_opts = [false, true];
 
-for perc = perc
+for perc = percs
     for is_sat = is_sat_opts
         run('./test_1_mass.m');
     end
