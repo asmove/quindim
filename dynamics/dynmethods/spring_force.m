b562c93981acd65bf0f8bb1e01ad46fe7a69fc01
Bruno <brunolnetto@gmail.com>
2020-02-18 20:26:27 -0300

    Master changes

---
+++ dynamics/dynmethods/spring_force.m
@@ -0,0 +1,17 @@
+function F_k = spring_force(sys, helper)
+    ks = [];
+    for body = sys.descrip.bodies
+        body = body{1};
+        n_k = length(body.springs);
+        springs_i = body.springs;
+        springs_i = springs_i{1};
+        for i = 1:n_k
+            ks = [ks; springs_i.k];
+        end
+    end
+    
+    ks = unique(ks);
+    
+    F_k = equationsToMatrix(helper.l_r, ks)*ks;
+    F_k = simplify_(F_k);
+end
