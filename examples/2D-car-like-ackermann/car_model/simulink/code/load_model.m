462ad8fd884d5bc2c36d9510f7f9b1bdb366b39b
Bruno <brunolnetto@gmail.com>
2020-11-17 10:51:47 -0300

    Model loader

---
+++ examples/2D-car-like-ackermann/car_model/simulink/code/load_model.m
@@ -0,0 +1,27 @@
+% Clear environment
+if(~exist('CLEAR_ALL'))
+    clear all;
+else
+    if(CLEAR_ALL)
+        clear all
+    end
+end
+
+SIM_SYS = true;
+
+close all
+clc
+
+% Setup
+run('./load_symvars.m');
+run('./load_transformations.m');
+run('./load_bodies.m');
+run('./load_params.m');
+
+% Build
+run('./load_consts.m');
+run('./load_kin.m');
+run('./load_dyn.m');
+
+% Simulink model
+run('./load_scripts.m');
--- examples/2D-car-like-ackermann/car_model/simulink/code/main.m
+++ examples/2D-car-like-ackermann/car_model/simulink/code/main.m
@@ -1,35 +1,12 @@
 % Author: Bruno Peixoto
 % Date: 08/01/19
 
-% Clear environment
-if(~exist('CLEAR_ALL'))
-    clear all;
-else
-    if(CLEAR_ALL)
-        clear all
-    end
-end
+% Load car model
+run('./load_model.m');
 
-SIM_SYS = true;
-
-close all
-clc
-
-% Setup
-run('./load_symvars.m');
-run('./load_transformations.m');
-run('./load_bodies.m');
-run('./load_params.m');
-
-% Build
-run('./load_consts.m');
-run('./load_kin.m');
-run('./load_dyn.m');
-
-% Loading and simulation
-run('./load_scripts.m');
+% Simulation
 run('./run_sim.m');
 
-% Visual preparation
+% Aesthetics
 run('./run_plot.m');
 run('./run_animation.m');
