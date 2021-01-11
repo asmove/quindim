d860209dcc07d3406f75002c30c7ea6117378127
Bruno <brunolnetto@gmail.com>
2021-01-11 15:20:23 -0300

    Clone libraries script

---
+++ clone_libs.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+mkdir ~/github/
+
+git clone https://github.com/brunolnetto/quindim
+git clone https://github.com/brunolnetto/matlab-utils
+git clone https://github.com/brunolnetto/baryopt
+
+ echo 'alias matlab=~/MATLAB/bin/matlab' >> ~/.bashrc
+
