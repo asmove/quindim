357957ee7f0510372ed84310c013991d8a90e808
Bruno <brunolnetto@gmail.com>
2021-01-11 15:20:37 -0300

    Library loader

---
+++ loadlibs.m
@@ -0,0 +1,12 @@
+PATHS = {'Robotics4fun', 'matlab-utils', 'baryopt'};
+
+GITPATH = '~/github/';
+
+for i = 1:3
+	libpath_i = [GITPATH, PATHS{i}];
+
+	addpath(libpath_i);
+	addpath(genpath(libpath_i));
+	savepath(libpath_i);
+end
+
